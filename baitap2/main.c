85c596c213f34a08524999275743ea5be33a9127
Anhdao1806 <daoleho123@gmail.com>
2025-02-06 19:49:35 +0700

    Update main.c

--- baitap2/main.c
+++ baitap2/main.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 int main() {
     FILE *file;
-    int f;
+    char f;
     file = fopen("baitap01.txt", "r");
     if (file == NULL) {
         printf("Loi mo file");
         return 1;
     }
     f = fgetc(file);
-    printf("Ky tu dau tien trong file: %d", f);
+    printf("Ky tu dau tien trong file: %c", f);
     fclose(file);
     return 0;
 }
